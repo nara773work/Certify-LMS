3af2002171782296d482e2a7793e53d95bd39e23
Your Name <nara773.work@gmail.com>
2026-08-22 16:45:56 +0900

    Add docs

--- app/UseCases/MockExam/IndexAction.php
+++ app/UseCases/MockExam/IndexAction.php
@@ -47,12 +47,6 @@ final class IndexAction
         }
 
         return $query
-            ->with([
-                'certification',
-                'createdBy',
-                'updatedBy',
-            ])
-            ->withCount('mockExamQuestions')
             ->orderBy('certification_id')
             ->orderBy('order')
             ->orderByDesc('updated_at')
